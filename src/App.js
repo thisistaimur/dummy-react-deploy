566774d63de4f9d6d81b2b09ef59e660db2aff46
thisistaimur <59065240+thisistaimur@users.noreply.github.com>
2021-03-23 10:23:05 +0100

    Source

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test project, we want to see how to re-deploy changes. 
         </p>
         <a
           className="App-link"
